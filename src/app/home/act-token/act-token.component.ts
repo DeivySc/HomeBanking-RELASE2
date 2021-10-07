ff33d002b214f247523c9713a3dff6dba209eb7d
DeivySc <sanchezcoronadodeivy5@gmail.com>
2021-10-07 18:03:06 -0500

    validaciones form control

--- src/app/home/act-token/act-token.component.ts
+++ src/app/home/act-token/act-token.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from "@angular/router";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-act-token',
@@ -7,11 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActTokenComponent implements OnInit {
 
-  checked!: boolean;
+  groupForm1!: FormGroup;
+  checked = new FormControl('', Validators.required);
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+    this.groupForm1 = new FormGroup({
+      check: this.checked,
+    })
+  }
+
+  public redirectConfig(){
+    this.router.navigateByUrl('/home/configuracion')
   }
 
 }
