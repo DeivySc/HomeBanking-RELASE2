499852fad516ba67b0828a374c2f331e4a2484c4
DeivySc <sanchezcoronadodeivy5@gmail.com>
2021-10-13 07:31:13 -0500

    animacion entrada step 1

--- src/app/home/act-token/act-token.component.ts
+++ src/app/home/act-token/act-token.component.ts
@@ -1,11 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {trigger, state, style, animate, transition} from "@angular/animations";
 
 @Component({
   selector: 'app-act-token',
   templateUrl: './act-token.component.html',
-  styleUrls: ['./act-token.component.scss']
+  styleUrls: ['./act-token.component.scss'],
+  animations: [
+    trigger('enterState', [
+      state('void',style({
+        transform: 'translateY(-100%)',
+        opacity:0.3
+      })),
+      transition(':enter', [
+        animate(5000, style({
+          transform: 'translateX(0)',
+          opacity:1
+        }))
+      ])
+    ])
+  ]
+
 })
 export class ActTokenComponent implements OnInit {
 
